127926338c8a9fa772e6f89cf83c661bffbbb9fc
Saki-0102 <saki030102@gmail.com>
2020-08-24 02:16:47 +0900

    asdf

--- functionTest.py
+++ functionTest.py
@@ -1,6 +1,9 @@
 import json, os, glob, binascii
 from Crypto.Hash import SHA256
 from Crypto.Cipher import Salsa20
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtGui import QIcon
 
 nowPath = str(os.getcwd())
 jsonPath = nowPath
@@ -8,64 +11,77 @@ jsonPath = nowPath
 jsonName = 'PasswordManager.json'
 print(jsonPath)
 os.chdir(jsonPath)
-
+'''
 
 with open(jsonName, 'r', encoding = 'utf-8') as json_file:
     data = json.load(json_file)
 
 masterPW = data["master"]
-
+'''
 
 def existMPW(jsonName):
     with open(jsonName, 'r', encoding = 'utf-8') as json_file:
         data = json.load(json_file)
 
+    print(data)
     masterPW = data["master"]
+    print(masterPW)
+    a = True
 
-    if masterPW == "":
-        print('타 사용자의 접근을 막기 위한 비밀번호를 설정합니다.')
-        a = True
+    if masterPW == '':
+        #print('타 사용자의 접근을 막기 위한 비밀번호를 설정합니다.')
+        a = False
 
-        while a == True:
+    return a
+
+        #while a == True:
+'''
             pw = input('설정할 비밀번호를 입력해주세요: ')
             pwRe = input('다시 한 번 입력해주세요: ')
             if pw == pwRe:
                 a = False
             else:
                 print('비밀번호가 일치하지 않습니다.')
+'''
 
-        #pw = 'masterpw'
-        h = SHA256.new()
-        h.update(bytearray(pw, encoding = 'utf-8'))
-        #print(h.hexdigest())
-        h.update(h.digest())
-        #print(h.hexdigest())
-        hashed = h.hexdigest()
-        data["master"] = hashed
-        with open(jsonName, 'w', encoding = 'utf-8') as json_f:
-            json.dump(data, json_f, indent=4, sort_keys=True)
+print(existMPW(jsonName))
+
+def SaveMPW(jsonName, mpw):
+#pw = 'masterpw'
+    with open(jsonName, 'r', encoding = 'utf-8') as json_file:
+        data = json.load(json_file)
+    h = SHA256.new()
+    h.update(bytearray(mpw, encoding = 'utf-8'))
+    #print(h.hexdigest())
+    h.update(h.digest())
+    #print(h.hexdigest())
+    hashed = h.hexdigest()
+    data["master"] = hashed
+    with open(jsonName, 'w', encoding = 'utf-8') as json_f:
+        json.dump(data, json_f, indent=4, sort_keys=True)
+
 
 #print(data)
 
 def makeJSON(jsonPath, jsonName):
     exist = False
     for i in glob.glob(jsonPath + '\\*'):
-        print(i[len(jsonPath)+1:])
+        #print(i[len(jsonPath)+1:])
         if i[len(jsonPath)+1:] == jsonName:
-            #exist = True
+            exist = True
             break
     #print(exist)
 
     if not exist:
-        print('makedir')
+        #print('makedir')
         pwData = {
         "master" : "",
         "mode" : "num",
         "min" : "60000",
         "num" : "1",
         "viewtime" : "30000",
-        "test" : {"샘플사이트" : {"id" : "example", "pw" : "example"}},
-        "view" : {"샘플사이트" : {"id" : "example", "pw" : "example"}},
+        "test" : {},
+        "view" : {},
         }
         with open(jsonName, 'w', encoding = 'utf-8') as json_file:
             json.dump(pwData, json_file, indent=4, sort_keys=True)
@@ -100,7 +116,7 @@ def input_password(readData, inputMPW, readMode, siteName, siteID, sitePW):
     with open(jsonName, 'w', encoding = 'utf-8') as json_f:
         json.dump(readData, json_f, ensure_ascii = False, indent=4, sort_keys=True)
 
-input_password(data, 'masterpw', 'b', "ThisisTest", "ThisisID", "ThisisPW")
+#input_password(data, 'masterpw', 'b', "ThisisTest", "ThisisID", "ThisisPW")
 
 
 
@@ -117,4 +133,4 @@ def read_password(readData, inputMPW, siteName):
     plainpw = sal.decrypt(pw_enc).decode('utf-8')
     print(plainpw)
 
-read_password(data, 'masterpw', 'ThisisTest')
+#read_password(data, 'masterpw', 'ThisisTest')
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+from Crypto.Hash import SHA256
 from Crypto.Cipher import Salsa20 as sal
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QAction, qApp
-from PyQt5.QtCore import QCoreApplication
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 import json, os, glob, sys
 import functionTest as ft
 
@@ -17,44 +18,145 @@ Password Manager for Windows
 '''
 
 
-class MyApp(QMainWindow):
+class MyApp(QWidget):
 
+    def __init__(self):
+        super().__init__()
+        self.checkJsonExist()
+        self.checkMPWexist()
+        self.initUI()
+
+    def initUI(self):
+        tab_test = TryTab()
+        tab_view = ViewTab()
+        tab_input = InputTab()
+        tab_setting = SettingTab()
+        tab_help = HelpTab()
+
+        tabs = QTabWidget()
+        tabs.addTab(tab_test, '입력 시도')
+        tabs.addTab(tab_view, '비밀번호 확인')
+        tabs.addTab(tab_input, '새로 입력')
+        tabs.addTab(tab_setting, '설정')
+        tabs.addTab(tab_help, '도움말')
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(tabs)
+
+        self.setLayout(vbox)
+
+        self.setWindowTitle('PasswordManager')
+        #self.move(300, 300)
+        self.resize(500, 400)
+        self.center()
+        self.show()
+
+    def checkJsonExist(self):
+        ft.makeJSON(jsonPath, jsonName)
+
+    def checkMPWexist(self):
+        with open(jsonName, 'r', encoding = 'utf-8') as json_file:
+            data = json.load(json_file)
+
+        exist = ft.existMPW(jsonName)
+        print(ft.existMPW(jsonName))
+        print(exist)
+
+        while exist == False:
+            newPW, ok = QInputDialog.getText(self, '비밀번호 입력', '사용할 프로그램 비밀번호를 입력하세요:')
+            PWagain, ok = QInputDialog.getText(self, '비밀번호 입력', '다시 입력해주세요:')
+            if newPW == '':
+                QMessageBox.question(self, '실패', '비밀번호는 공백일 수 없습니다.', QMessageBox.Yes)
+                continue
+            elif newPW == PWagain:
+                QMessageBox.question(self, '성공', '비밀번호를 설정했습니다.', QMessageBox.Yes)
+                ft.SaveMPW(jsonName, newPW)
+                exist = True
+            else:
+                reply = QMessageBox.question(self, '실패', '비밀번호가 일치하지 않습니다. \n비밀번호를 설정하지 않으면 프로그램이 종료됩니다. \n비밀번호를 설정하시겠습니까?', QMessageBox.Yes | QMessageBox.No, QMessageBox.Yes)
+                if reply == QMessageBox.No:
+                    sys.exit()
+                else:
+                    continue
+
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, 'Message', '정말 종료하시겠습니까?', QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+class TryTab(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
-        self.statusBar().showMessage('testing')
+        sitename = QLabel('사이트 이름:')
+        siteNameCB = QComboBox(self)
+        self.loadComboBox()
+        sumbitButton = QPushButton('Sumbit')
 
-        exitAction = QAction('Exit', self)
-        exitAction.setShortcut('Ctrl+Q')
-        exitAction.setStatusTip('Exit application')
-        exitAction.triggered.connect(qApp.quit)
+        grid = QGridLayout()
+        grid.addWidget(sitename, 0, 0)
+        grid.addWidget(siteNameCB, 0, 1)
+        grid.addWidget(sumbitButton, 1, 1)
 
-        settingAction = QAction(QIcon('\\setting.png'), 'Exit', self)
-        settingAction.setShortcut('Esc')
-        settingAction.setStatusTip('Open Setting Window')
-        #settingAction.triggered.connect(qApp.)
+        self.setLayout(grid)
 
-        self.statusBar()
+    def loadComboBox(self):
+        with open(jsonName, 'r', encoding = 'utf-8') as json_file:
+            data = json.load(json_file)
 
-        menubar = self.menuBar()
-        menubar.setNativeMenuBar(False)
-        filemenu = menubar.addMenu('&File')
-        filemenu.addAction(exitAction)
-        filemenu.addAction(settingAction)
+        siteNameList = list(data["test"].keys())
+        for i in siteNameList:
+            siteNameCB.addItem(i)
 
-        '''
-        btn = QPushButton('Quit', self)
-        btn.move(900, 400)
-        btn.resize(btn.sizeHint())
-        btn.clicked.connect(QCoreApplication.instance().quit)
-        '''
+    def inputMPQ(self):
+        mPW, ok = QInputDialog.getText(self, 'Input MasterPassword', 'Enter Password:')
 
-        self.setWindowTitle('PasswordManager')
-        #self.move(300, 300)
-        self.resize(1000,500)
-        self.show()
+class ViewTab(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        grid = QGridLayout()
+        self.setLayout(grid)
+
+class InputTab(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        grid = QGridLayout()
+        self.setLayout(grid)
+
+class SettingTab(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        grid = QGridLayout()
+        self.setLayout(grid)
+
+class HelpTab(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        grid = QGridLayout()
+        self.setLayout(grid)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
